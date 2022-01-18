9a8783cab7338da5401a6291ae9e4ab0823d91ef
Tran <tran.van.sang@sun-asterisk.com>
2022-01-18 15:55:45 +0700

    gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+  end
 end
