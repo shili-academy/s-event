eb325c63acf2c0ddb7821404f70e78e56dc04a5b
Tran <tran.van.sang@sun-asterisk.com>
2022-01-18 16:59:40 +0700

    CSS FW

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  as :user do
-    get "signin" => "devise/sessions#new"
-    post "signin" => "devise/sessions#create"
-    delete "signout" => "devise/sessions#destroy"
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+    devise_for :users
+    as :user do
+      get "signin" => "devise/sessions#new"
+      post "signin" => "devise/sessions#create"
+      delete "signout" => "devise/sessions#destroy"
+    end
   end
 end
