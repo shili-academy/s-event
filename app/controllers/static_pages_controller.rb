1b504ddacbf7bc39573df86196eabeadd6813704
Tran <tran.van.sang@sun-asterisk.com>
2022-01-18 23:41:21 +0700

    A

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    flash[:alert] = "authenticate_user authenticate_user authenticate_user"
   end
 end
